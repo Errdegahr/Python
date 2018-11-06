1a0ed9c1e84f92ef49a47a77a5d3336452b150f2
Errdegahr <Errdegahr2@yandex.ru>
2018-11-07 02:46:29 +0300

    Lesson6

---
+++ Lesson6/easy6_1-2.py
@@ -0,0 +1,87 @@
+#Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
+# Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+
+
+import math
+
+class Triangle:
+    def __init__(self, A, B, C):
+        #длина стороны согласно координатам точек
+        def sideLen(dot1, dot2):
+            return math.sqrt((dot1[0] - dot2[0]) ** 2
+                             + (dot1[1] - dot2[1]) ** 2)
+
+        self.A = A
+        self.B = B
+        self.C = C
+        #длина стороны AB
+        self.AB = sideLen(self.A, self.B)
+        self.BC = sideLen(self.B, self.C)
+        self.CA = sideLen(self.C, self.A)
+
+    #площади треугольника
+    def area(self):
+        semi_perimeter = self.perimeter() / 2
+
+        return math.sqrt(semi_perimeter
+                         * (semi_perimeter - self.AB)
+                         * (semi_perimeter - self.BC)
+                         * (semi_perimeter - self.CA))
+
+    #периметр треугольника
+    def perimeter(self):
+        return self.AB + self.BC + self.CA
+
+
+    def height(self):
+        return self.area() / (self.AB / 2)
+
+
+FTriangle = Triangle((4, 3), (7, 8), (0, 10))
+
+print(FTriangle.area())
+print(FTriangle.height())
+print(FTriangle.perimeter())
+
+
+# Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
+# Предусмотреть в классе методы:
+# проверка, является ли фигура равнобочной трапецией;
+# вычисления: длины сторон, периметр, площадь.
+
+class Trapez:
+    def __init__(self, A, B, C, D):
+        #длина стороны согласно координатам точек
+        def sideLen(dot1, dot2):
+            return math.sqrt((dot1[0] - dot2[0]) ** 2
+                             + (dot1[1] - dot2[1]) ** 2)
+
+        def area(len1, len2, len3):
+            semi_perimeter = (len1 + len2 + len3) / 2
+
+            return math.sqrt(semi_perimeter
+                             * (semi_perimeter - len1)
+                             * (semi_perimeter - len2)
+                             * (semi_perimeter - len3))
+
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+
+        self.AB = sideLen(self.A, self.B)
+        self.BC = sideLen(self.B, self.C)
+        self.CD = sideLen(self.C, self.D)
+        self.DA = sideLen(self.D, self.A)
+        self.diagonal_AC = sideLen(self.C, self.A)
+        self.diagonal_BD = sideLen(self.B, self.D)
+        self.perimeter = self.AB + self.BC + self.CD + self.DA
+
+        #предположим, что наша трапеция - это два треугольника
+        self.area = area(self.AB, self.diagonal_BD, self.DA) \
+                    + area(self.diagonal_BD, self.BC, self.CD)
+
+    def isTrapezeEqu(self):
+        if self.diagonal_AC == self.diagonal_BD:
+            return True
+        return False
